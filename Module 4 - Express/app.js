8d722b8fc2a8299d59e060a6e568def533aa5716
Pedro Meireles <pedmeireles@gmail.com>
2021-03-28 11:57:42 -0300

    Update app.js

--- Module 4 - Express/app.js
+++ Module 4 - Express/app.js
@@ -6,13 +6,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/user');
+const userRoutes = require('./routes/user');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use(userRoutes);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
